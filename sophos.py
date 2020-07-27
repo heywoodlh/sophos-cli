f7e22f7b66c0b0bd8de8e8cded42885a8e59b24a
Spencer Heywood <l.spencer.heywood@protonmail.com>
2020-07-27 16:06:24 -0600

    basic sophos.py

---
+++ sophos.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import argparse
+import requests
+
+## argparse arguments
+parser = argparse.ArgumentParser(description="Wrapper for Sophos Central's API")
+parser.add_argument('--api', help='Sophos Central API token', metavar='TOKEN', required=True)
+parser.add_argument('-g', '--gateway', help='Sophos Central API Gateway', metavar='GATEWAY', required=True)
+parser.add_argument('--auth', help='Sophos Central Basic Auth Token', metavar='TOKEN', required=True)
+subparsers = parser.add_subparsers(help='commands', dest='command')
+
+## Events subparser
+parser_events = subparsers.add_parser('events', help='Retrieve sophos central event info')
+
+args = parser.parse_args()
+
+
+def main():
+    gateway = args.gateway
+    apikey = args.api
+    authtoken = args.auth
+
+    headers = { 
+    'x-api-key': apikey, 
+    'Authorization': 'Basic ' + authtoken
+    }
+    
+    if args.command == 'events':
+        gateway = gateway + '/siem/v1/events'
+    response = requests.get(gateway, headers=headers)
+    print(response.text)
+
+
+
+if __name__ == '__main__':
+    main()
