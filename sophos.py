73e4ee36ea5e35ec9716997400003347750ccdfd
Spencer Heywood <l.spencer.heywood@protonmail.com>
2020-07-30 15:24:36 -0600

    updated

--- sophos.py
+++ sophos.py
@@ -1,37 +1,38 @@
 #!/usr/bin/env python3
 import argparse
 import requests
+import sys
 
 ## argparse arguments
 parser = argparse.ArgumentParser(description="Wrapper for Sophos Central's API")
-parser.add_argument('--api', help='Sophos Central API token', metavar='TOKEN', required=True)
-parser.add_argument('-g', '--gateway', help='Sophos Central API Gateway', metavar='GATEWAY', required=True)
-parser.add_argument('--auth', help='Sophos Central Basic Auth Token', metavar='TOKEN', required=True)
+parser.add_argument('--gateway', help='Sophos API URI', metavar='URI', default='https://api-us01.central.sophos.com')
+parser.add_argument('--tenant-id', help='Sophos Central Tenant ID', metavar='ID', required=True)
+parser.add_argument('--access-token', help='Sophos Central Access Token', metavar='TOKEN', required=True)
 subparsers = parser.add_subparsers(help='commands', dest='command')
 
-## Events subparser
-parser_events = subparsers.add_parser('events', help='Retrieve sophos central event info')
 
-## Alerts subparser
-parser_events = subparsers.add_parser('alerts', help='Retrieve sophos central alert info')
+## Endpoints subparser
+parser_events = subparsers.add_parser('endpoints', help='Retrieve sophos central endpoint info')
 
 args = parser.parse_args()
 
 
 def main():
     gateway = args.gateway
-    apikey = args.api
-    authtoken = args.auth
+    tenant_id = args.tenant_id
+    access_token = args.access_token
+
+
+    authorization_string = "Bearer " + access_token
 
     headers = {
-    'x-api-key': apikey,
-    'Authorization': 'Basic ' + authtoken
+    'Authorization': authorization_string,
+    'X-Tenant-ID': tenant_id
     }
 
-    if args.command == 'events':
-        gateway = gateway + '/siem/v1/events'
-    if args.command == 'alerts':
-        gateway = gateway + '/siem/v1/alerts'
+
+    if args.command == 'endpoints':
+        gateway = gateway + '/endpoint/v1/endpoints'
     response = requests.get(gateway, headers=headers)
     print(response.text)
 
